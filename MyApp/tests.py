3f28092e18dc8ec13a88b4ad53fa86d3fdc65980
Sudeesh E S <117679137+Sudeesh404@users.noreply.github.com>
2023-12-17 22:06:03 +0530

    test

--- MyApp/tests.py
+++ MyApp/tests.py
@@ -44,13 +44,42 @@ class Logintest(LiveServerTestCase):
         print("Test scenario 'Users Login passed.")
 
     def test_feedback_submission(self):
-        feedback_url = self.live_server_url + 'http://127.0.0.1:8000/submit_feedback/'  # Change to your actual feedback submission page URL
+    feedback_url = self.live_server_url + '/feedback/'  # Change to your actual feedback submission page URL
 
-        # Navigate to the feedback submission page
-        self.driver.get(feedback_url)
-        self.driver.find_element(By.ID, "feedback_text").send_keys("This is a test feedback.")
-        self.driver.find_element(By.ID, "submit_button").click()
-        print("Test scenario 'Feedback Submission' passed.")
+    # Navigate to the feedback submission page
+    self.driver.get(feedback_url)
+
+    try:
+        # Wait for the feedback text input field to be visible
+        WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((By.ID, "feedback_text"))
+        )
+    except TimeoutException as e:
+        # Output more information to help diagnose the issue
+        print(f"TimeoutException: {e}")
+        print(f"Current URL: {self.driver.current_url}")
+        print(f"Current Title: {self.driver.title}")
+        raise
+
+    # Try to find and interact with the feedback text input field
+    try:
+        feedback_text_input = self.driver.find_element(By.ID, "feedback_text")
+        feedback_text_input.send_keys("This is a test feedback.")
+    except NoSuchElementException as e:
+        # Output more information to help diagnose the issue
+        print(f"NoSuchElementException: {e}")
+        print(f"Current URL: {self.driver.current_url}")
+        print(f"Current Title: {self.driver.title}")
+        raise
+
+    # Add more assertions or checks based on the behavior of your feedback submission
+    # For example, check for a success message or verify if the feedback is displayed on the page
+
+    # Add assertions here based on the structure of your feedback submission page
+    # Example:
+    # self.assertTrue(self.driver.find_element(By.ID, "success_message").is_displayed())
+
+    print("Test scenario 'Feedback Submission' passed.")
 
 if __name__ == '__main__':
     LiveServerTestCase.main()
