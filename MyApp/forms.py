48b0ddc4c97e355c7f91827f254f31e7eaadd62f
Sudeesh E S <117679137+Sudeesh404@users.noreply.github.com>
2024-02-20 09:27:06 +0530

    x

--- MyApp/forms.py
+++ MyApp/forms.py
@@ -85,6 +85,8 @@ class PoliceStationRegistrationForm(UserCreationForm):
                 first_name=self.cleaned_data['first_name'],
                 last_name=self.cleaned_data['last_name'],
                 email=self.cleaned_data['email'],
-                branch=self.cleaned_data['branch']
+                branch=self.cleaned_data['branch'],
+                password1=self.cleaned_data['password1'],
+                password2=self.cleaned_data['password2']
             )
         return user
